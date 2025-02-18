1f6f716ba96f90da7ea15677232685e2d74dcef4
shenumd123 <shenumd7@gmail.com>
2025-02-18 07:54:31 +0530

    Update movie.js

--- plugins/movie.js
+++ plugins/movie.js
@@ -50,7 +50,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         // Send the movie information along with the poster image
         await conn.sendMessage(from, {
             image: { url: imageUrl },
-            caption: `${movieInfo}\n> ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴇᴛʜᴜ ᴍᴀx`
+            caption: `${movieInfo}\n> ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɢʜᴏꜱᴛ ᴛᴇᴀᴍ`
         }, { quoted: mek });
     } catch (e) {
         console.log(e);
