18be4961ffdc0c451bc2fbdfbed3919dd0fa326b
shenumd123 <shenumd7@gmail.com>
2025-02-18 07:53:41 +0530

    Update hack.js

--- plugins/hack.js
+++ plugins/hack.js
@@ -36,7 +36,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
             '⚠️ *Note:* All actions are for demonstration purposes only.',
             '⚠️ *Reminder:* Ethical hacking is the only way to ensure security.',
             '',
-            '> *NETHU-MD-HACKING-COMPLETE ☣*'
+            '> *DARK HACKER MD-HACKING-COMPLETE ☣*'
         ];
 
         for (const line of steps) {
